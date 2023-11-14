c6348a1d3d4b5cd98bf85af580998d38fe8033d0
EderdaSilva <ederanton@gmail.com>
2023-11-13 23:17:17 -0300

    finalizado codigo Heroi com While

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log ("HE to On")
+let nomeHeroi = "StarFelip"
+let NivelXP = 60010
+let msgNivel = `O herói de Nome ${nomeHeroi}, está no nivel: ${NivelXP}, e sua classificação é: `
+
+while( NivelXP <=1000){
+    console.log (msgNivel + "Ferro")
+    break
+}
+while( NivelXP >1000 && NivelXP <=2000 ){
+    console.log (msgNivel + "Broze")
+    break
+}
+while( NivelXP >2000 && NivelXP <=6000 ){
+    console.log (msgNivel + "Prata")
+    break
+}
+while( NivelXP >6000 && NivelXP <=7000 ){
+    console.log (msgNivel + "Ouro")
+    break
+}
+while( NivelXP >7000 && NivelXP <=8000 ){
+    console.log (msgNivel + "Platina")
+    break
+}
+while( NivelXP >8000 && NivelXP <=9000 ){
+    console.log (msgNivel + "Ascendente")
+    break
+}
+while( NivelXP >9000 && NivelXP <=10000 ){
+    console.log (msgNivel + "Imortal")
+    break
+}
+while( NivelXP >10000 ){
+    console.log (msgNivel + "Radiante")
+    break
+}
